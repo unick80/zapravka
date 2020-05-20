fd599e6db731526aa75c7cbcb96eebf47189dc5e
Nick Pisarev <zagis-net@mail.ru>
2020-05-20 14:14:39 +0300

    Updates

---
+++ precache-manifest.1686703f22f52262d52b5e4734aff23f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25e2e337e4e726081c880880675ae569",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d6b8c5a70c0c936e038b",
+    "url": "/static/css/2.86df01db.chunk.css"
+  },
+  {
+    "revision": "fdc6c3cfaa683af111cd",
+    "url": "/static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "d6b8c5a70c0c936e038b",
+    "url": "/static/js/2.65210d8c.chunk.js"
+  },
+  {
+    "revision": "21ced859ea2b2d6b856d461ad6c2afed",
+    "url": "/static/js/2.65210d8c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fdc6c3cfaa683af111cd",
+    "url": "/static/js/main.c0205707.chunk.js"
+  },
+  {
+    "revision": "7ee1f3a719140dc597cc",
+    "url": "/static/js/runtime-main.494f48a5.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/static/media/persik.4e1ec840.png"
+  }
+]);
